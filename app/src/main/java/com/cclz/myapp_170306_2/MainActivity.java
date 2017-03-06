c3eaae5234f5d46a2bbe9202546a7456cd02fec6
Cclz <rex620620>
2017-03-06 10:59:38 +0800

    Button OnClick

--- app/src/main/java/com/cclz/myapp_170306_2/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170306_2/MainActivity.java
@@ -2,6 +2,9 @@ package com.cclz.myapp_170306_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn=(Button) findViewById(R.id.button); //指向畫面上的按鈕
+        MyClass c=new MyClass();
+        btn.setOnClickListener(c);// 設定一個符合介面的物件
+
+    }
+    class MyClass implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Log.d("BTN","Test1");
+        }
     }
 
 
