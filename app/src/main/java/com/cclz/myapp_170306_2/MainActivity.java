418b12ebfc473827d0aeadf4728306a53a2dd079
Cclz <rex620620>
2017-03-06 11:18:46 +0800

    2 Button

--- app/src/main/java/com/cclz/myapp_170306_2/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170306_2/MainActivity.java
@@ -7,17 +7,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn=(Button) findViewById(R.id.button); //指向畫面上的按鈕
+        btn=(Button) findViewById(R.id.button); //指向畫面上的按鈕
+        btn2=(Button) findViewById(R.id.button2);
         btn.setOnClickListener(new View.OnClickListener(){// 設定一個符合介面的物件
             @Override
             public void onClick(View v) {
                 Log.d("BTN","Test1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){// 設定一個符合介面的物件
+            @Override
+            public void onClick(View v) {
+                Log.d("BTN","Test2");
             }
         });
     }
